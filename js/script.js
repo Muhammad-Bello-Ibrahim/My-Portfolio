014f8cd1db241acbd5c256f3e53a09cb38768793
Aminu Ahmad <aminu@strano.dev>
2023-03-12 19:03:47 +0100

    2nd commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
-var typed = new typed(".multiple-text", {
-    strings: ["Frontend Developer", "Graphic Designer", "Marketer", 
-    "Youtuber"],
-typeSpeed: 100,
-backSpeed: 100,
-backDelay: 1000,
-loop: true
-})
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "Graphic Designer", "Mobile Developer",
+        "Best fit for your Projects"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+// var typed2 = new Typed('#typed2', {
+//     strings: ['Some <i>strings</i> with', 'Some <strong>HTML</strong>', 'Chars &times; &copy;'],
+//     typeSpeed: 0,
+//     backSpeed: 0,
+//     fadeOut: true,
+//     loop: true
+//   });
