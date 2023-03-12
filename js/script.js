2b5a30a4aa4ad8d00f843b20a259c77dfbfb80ea
Mufteem <mr.zplux009@gmail.com>
2023-03-12 16:21:39 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var typed = new typed(".multiple-text", {
+    strings: ["Frontend Developer", "Graphic Designer", "Marketer", 
+    "Youtuber"],
+typeSpeed: 100,
+backSpeed: 100,
+backDelay: 1000,
+loop: true
+})
